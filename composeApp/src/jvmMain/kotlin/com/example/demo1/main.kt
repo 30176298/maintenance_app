a5d3086fa4399508ce11efe6d153125921a529fa
Snake <187502961+30176298@users.noreply.github.com>
2025-12-17 10:33:32 +0000

    Boilerplate application

---
+++ composeApp/src/jvmMain/kotlin/com/example/demo1/main.kt
@@ -0,0 +1,13 @@
+package com.example.demo1
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "demo1",
+    ) {
+        App()
+    }
+}
